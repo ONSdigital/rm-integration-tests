8028aeb8868c79ba113818888864cc931e73c27a
Stephen Goddard <stephen.goddard@ons.gov.uk>
2017-07-05 13:30:00 +0100

    CTPA-1378 tidy test classes and amend comments where required

--- src/test/java/uk/gov/ons/ctp/journeys/execute/exercise/specs/ExecuteCollectionExerciseTest.java
+++ src/test/java/uk/gov/ons/ctp/journeys/execute/exercise/specs/ExecuteCollectionExerciseTest.java
@@ -9,11 +9,11 @@ import cucumber.api.junit.Cucumber;
  * Created by stephen.goddard on 25/05/17.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"classpath:uk/gov/ons/ctp/journeys/publish/exercise/publishCollectionExercise.feature"},
+@CucumberOptions(features = {"classpath:uk/gov/ons/ctp/journeys/execute/exercise/executeCollectionExercise.feature"},
   glue = {"uk.gov.ons.ctp.response.samplesvc.steps",
           "uk.gov.ons.ctp.response.collectionexercisesvc.steps",
           "uk.gov.ons.ctp.response.common.steps"},
-  plugin = {"pretty", "html:build/publishExercise-cuc-html-report", "json:build/jenkins/publishExercise.json"}
+  plugin = {"pretty", "html:build/executeExercise-cuc-html-report", "json:build/jenkins/executeExercise.json"}
 )
 public class ExecuteCollectionExerciseTest {
 
--- src/test/java/uk/gov/ons/ctp/smoke/specs/SmokeTest.java
+++ src/test/java/uk/gov/ons/ctp/smoke/specs/SmokeTest.java
@@ -13,6 +13,7 @@ import cucumber.api.junit.Cucumber;
   features = {"classpath:uk/gov/ons/ctp/smoke/smoke.feature"},
   glue = {"uk.gov.ons.ctp.response.samplesvc.steps",
           "uk.gov.ons.ctp.response.collectionexercisesvc.steps",
+          "uk.gov.ons.ctp.response.actionexporter.steps",
           "uk.gov.ons.ctp.response.common.steps"},
   plugin = {"pretty", "html:build/smoke-cuc-html-report", "json:build/jenkins/smoke.json"}
 )
