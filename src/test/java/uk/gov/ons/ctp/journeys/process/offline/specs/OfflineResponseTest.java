88d5bed5e3d32187879b2402143403abc2e8cde8
Stephen Goddard <stephen.goddard@ons.gov.uk>
2017-08-18 10:35:24 +0100

    CTPA-1384 Add offline response Journey 11

---
+++ src/test/java/uk/gov/ons/ctp/journeys/process/offline/specs/OfflineResponseTest.java
@@ -0,0 +1,21 @@
+package uk.gov.ons.ctp.journeys.process.offline.specs;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Created by stephen.goddard on 17/08/17.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"classpath:uk/gov/ons/ctp/journeys/process/offline/offlineResponse.feature"},
+  glue = {"uk.gov.ons.ctp.response.samplesvc.steps",
+      "uk.gov.ons.ctp.response.collectionexercisesvc.steps",
+      "uk.gov.ons.ctp.response.casesvc.steps",
+      "uk.gov.ons.ctp.response.common.steps"},
+  plugin = {"pretty", "html:build/OfflineResponse-report", "json:build/jenkins/OfflineResponse.json"}
+)
+public class OfflineResponseTest {
+
+}
