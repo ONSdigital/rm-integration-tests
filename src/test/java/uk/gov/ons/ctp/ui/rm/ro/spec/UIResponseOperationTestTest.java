16aff2f6e8c3f2aa0791738eab57830edb12ce7d
Stephen Goddard <stephen.goddard@ons.gov.uk>
2017-10-10 08:23:24 +0100

    Fix complile issues

--- src/test/java/uk/gov/ons/ctp/ui/rm/ro/spec/UIResponseOperationTestTest.java
+++ src/test/java/uk/gov/ons/ctp/ui/rm/ro/spec/UIResponseOperationTestTest.java
@@ -10,10 +10,10 @@ import cucumber.api.junit.Cucumber;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-  features = {"classpath:uk/gov/ons/ctp/ui/rm/ro/uiTest.feature"},
+  features = {"classpath:uk/gov/ons/ctp/ui/rm/ro/uiResponseOperationTest.feature"},
   glue = {"uk.gov.ons.ctp.ui.rm.ro.steps"},
-  plugin = {"pretty", "html:build/toBeNamed-cuc-html-report", "json:build/jenkins/toBeNamed.json"}
+  plugin = {"pretty", "html:build/uiRO-cuc-html-report", "json:build/jenkins/uiRO.json"}
 )
-public class UiTestTest {
+public class UIResponseOperationTestTest {
 
 }
